a68d90d0282265cca5b93c98b3da8294951a0112
Biktwp <victor.gonzalez@optivamedia.com>
2021-11-13 15:46:19 +0100

    Connect mongodb to flask

--- website/app/views/main.py
+++ website/app/views/main.py
@@ -2,19 +2,25 @@ from flask import (
     Blueprint, flash, render_template, make_response, session,
     request, current_app as app, url_for, abort, redirect
 )
+import locale
 
 import json
 
-
 main_module = Blueprint('main', __name__, template_folder='../templates')
 
-
+locale.setlocale(locale.LC_ALL, '')
 @main_module.route('/', methods=['GET'])
 def index():
     """ Página principal
     """
-    return render_template('index.html')
+    all_contracts = app.mongo.get_all_contracts()
+    category_list = [(i, len(app.mongo.get_contracts_by_category(i))) for i in
+                     app.mongo.get_contracts_categories()]
+    category_list.sort(key=lambda x: x[1], reverse=True)
+    total_money = sum(map(lambda j: j['presupuesto'] if 'presupuesto' in j.keys() else 0, all_contracts)), 2
 
+    return render_template('index.html', numero_contratos=len(all_contracts),
+                           categorias=list(category_list), dinero_total=locale.format_string("%.2f",total_money,grouping=True))
 
 @main_module.route('/results', methods=['POST'])
 def results():
@@ -24,4 +30,7 @@ def results():
         REFERENCIA DE QUÉ NECESITO EN EL FRONT: https://github.com/Patataman/DMyE1/issues/10
     """
     busqueda = request.form['busqueda']
-    return render_template('results.html', busqueda=busqueda, contratos=[1]*30)
+    print(busqueda)
+    search = app.mongo.get_contracts_by_title(busqueda)
+    print(search)
+    return render_template('results.html', busqueda=busqueda, contratos=search)
