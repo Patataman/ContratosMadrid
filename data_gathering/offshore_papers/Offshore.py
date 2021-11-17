bd833d0085e4fc4168d95d8d9d41c19356df22e8
christianmg99 <46071367+christianmg99@users.noreply.github.com>
2021-11-17 01:32:58 +0100

    Add files via upload

---
+++ data_gathering/offshore_papers/Offshore.py
@@ -0,0 +1,21 @@
+from neo4j import GraphDatabase
+import json
+
+driver = GraphDatabase.driver("bolt://127.0.0.1:7687", auth=("neo4j", "password"))
+
+def print_officers(tx, country):
+    data = set()
+    for record in tx.run("MATCH (n:Officer) WHERE n.country_codes CONTAINS $country RETURN DISTINCT toLower(n.name)", country = country):
+        data.add(record.values()[0])
+    for record in tx.run("MATCH (n:Intermediary) WHERE n.country_codes CONTAINS $country RETURN DISTINCT toLower(n.name)", country = country):
+        data.add(record.values()[0])
+    for record in tx.run("MATCH (n:Entity) WHERE n.country_codes CONTAINS $country RETURN DISTINCT toLower(n.name)", country = country):
+        data.add(record.values()[0])
+    data = list(data)
+    with open('offshore_papers.json', 'w') as outfile:
+        json.dump(data, outfile)
+
+with driver.session() as session:
+    session.read_transaction(print_officers, "ESP")
+
+driver.close()
