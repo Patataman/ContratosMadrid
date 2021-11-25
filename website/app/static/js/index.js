204cf62e421d9fb33a7277b71cab87ed68e1d37d
Juanbanpar <100383373@alumnos.uc3m.es>
2021-11-25 22:54:12 +0100

    Maps has some kind of offset but it works

--- website/app/static/js/index.js
+++ website/app/static/js/index.js
@@ -1,33 +1,37 @@
 $(document).ready(function() {
-    console.log(companies_locations);
-    /* Data points defined as an array of LatLng objects */
-    var heatmapData = [
-        new google.maps.LatLng(37.782, -122.447),
-        new google.maps.LatLng(37.782, -122.445),
-        new google.maps.LatLng(37.782, -122.443),
-        new google.maps.LatLng(37.782, -122.441),
-        new google.maps.LatLng(37.782, -122.439),
-        new google.maps.LatLng(37.782, -122.437),
-        new google.maps.LatLng(37.782, -122.435),
-        new google.maps.LatLng(37.785, -122.447),
-        new google.maps.LatLng(37.785, -122.445),
-        new google.maps.LatLng(37.785, -122.443),
-        new google.maps.LatLng(37.785, -122.441),
-        new google.maps.LatLng(37.785, -122.439),
-        new google.maps.LatLng(37.785, -122.437),
-        new google.maps.LatLng(37.785, -122.435)
-    ];
-
+    let companies_locations_json = JSON.parse(companies_locations);
+    console.log(companies_locations_json);
+    
     var madrid = new google.maps.LatLng(40.5248319, -3.771562775451811);
-
     var map = new google.maps.Map(document.getElementById('map'), {
         center: madrid,
         zoom: 7,
         mapTypeId: 'roadmap'
     });
-
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-        data: heatmapData
-    });
-    heatmap.setMap(map);
+    
+    var heatmapData = [];
+    
+    for (loc in companies_locations_json) {
+        //let loc = "Toledo"
+        let url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + companies_locations_json[loc]._id + "&region=es&key=";
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            for(var i = 0; i < companies_locations_json[loc].count; i++) {
+                heatmapData.push(new google.maps.LatLng(
+                    parseInt(data.results[0].geometry.location.lat), 
+                    parseInt(data.results[0].geometry.location.lng)
+                ))
+            }
+            
+            var heatmap = new google.maps.visualization.HeatmapLayer({
+                data: heatmapData
+            });
+            heatmap.setMap(map); 
+        })
+        .catch(function(error) {
+            console.log('Hubo un problema con la petición Fetch:' + error.message);
+        });
+    }
+    
 });
